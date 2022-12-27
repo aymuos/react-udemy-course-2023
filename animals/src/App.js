65220739c3424d82e4e154704058d2890a3a7b35
Aymuos <soumyamukherjee@mail.com>
2022-12-28 02:23:42 +0530

    upto useState

--- animals/src/App.js
+++ animals/src/App.js
@@ -3,16 +3,18 @@ import { useState} from "react";
 function App(){
     {/* arrow function on buttons clicked */}
     {/* these functions are called event handler or callback function that has to pass over a valid event name like onMouseHover , or onClick */}
-
+    const [count,setCount] = useState(0);
+    {/*usestate value is the default value */}
     const handleClick = () => {
-
-        console.log("Button was Clicked !")
+        {/* inside the event handler we update the state */}
+        setCount(count+1);
     };
     return (
         <div>
-            <button onClick={()=> console.log("Button was clicked 2")}>
+            <button onClick={handleClick}>
                 Add Animal
             </button>
+            <div> Number of Animals : {count}</div>
         </div>
 
     )
